26058bb5cc5a9418d7aff697b19d6b58979f806e
rafaseoanes <rafaseoanes9@gmail.com>
2023-09-07 09:26:09 -0500

    excercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToChange, ...toRemove) {
 
+    // for(let i = 0; i < arrayToChange.length; i++){
+    //     if(arrayToChange[i] === toRemove[i]){
+            
+    //     }
+    // }
+
+    ([1,2, 3], 3, 2, 1)
+
+    return arrayToChange.filter(elementoActual =>{
+     
+        return !toRemove.includes(elementoActual);
+     
+    })
    
 };
 
