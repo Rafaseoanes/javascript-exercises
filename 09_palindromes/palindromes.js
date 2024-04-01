88916d45c1af94dd4426038a95da2eb7e98ceea4
rafaseoanes <rafaseoanes9@gmail.com>
2024-04-01 11:40:46 -0500

    excercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+    let cleanedText = word.toLowerCase().split('')
+
+    for (let i = 0; i < cleanedText.length; i++) {
+        let val = cleanedText[i];
+        if (val == ' ' || val == ',' || val == '!' || val == '.') { // ERROR
+            cleanedText.splice(i, 1);
+            i--;
+        }
+
+    }
+
+    let reversedWord = [...cleanedText]
+
+    let finalText = reversedWord.reverse().join('').toLowerCase()
+
+    // console.log(cleanedText, finalText)
+
+
+    return finalText == cleanedText.join('')
 
 };
 
