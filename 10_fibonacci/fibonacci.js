7545662c8a81b73ed6424066bcd0c2b5a88d3f76
rafaseoanes <rafaseoanes9@gmail.com>
2024-04-04 17:02:09 -0500

    excercise #9 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    parseInt(num)
+    let fibSec = [0, 1]
+
+    if (num < 0) {
+        return "OOPS"
+    } else {
+        for (let i = 1; i <= num; i++) {
+
+            fibSec[i + 1] = fibSec[i] + fibSec[i - 1]
+        }
+
+        return fibSec[num]
+    }
 
 };
 
