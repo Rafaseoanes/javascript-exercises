ee0127366dd1e3e5d58ed67fd5c97ea177014aa3
rafaseoanes <rafaseoanes9@gmail.com>
2024-04-04 21:08:35 -0500

    Exercise #12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+
+     let oldest = people.map((person)=>{
+
+        if(person.yearOfDeath == undefined){
+            person.yearOfDeath = new Date().getFullYear()
+        }
+
+        let age = person.yearOfDeath - person.yearOfBirth
+         
+        return {
+            ...person,
+            age: age
+        }
+
+     }).sort((a, b) =>{    
+        return b.age - a.age
+    })
+
+
+    
+    return oldest[0]
 
 };
 
